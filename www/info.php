3d02a9c16631c0284a47a2314470f0619bd339bc
John Rayes <live627@gmail.com>
2026-05-30 22:09:54 -0700

    Delete www directory

--- www/info.php
+++
@@ -1,8 +0,0 @@
-<?php
-	ob_start();
-	phpinfo(INFO_GENERAL | INFO_CONFIGURATION | INFO_MODULES);
-    $phpinfo = array();
-
-	$parts = preg_split('~(<[^>]+>)~', ob_get_clean(), -1, PREG_SPLIT_DELIM_CAPTURE);
-
-    var_dump($parts);
